9f242e29eea450f7e09677266adb0f74363b8c69
Annie Liao <liaowow@gmail.com>
2020-10-06 14:42:11 -0400

    install nodemon to enable automatic reloading of the node server

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 9000
 
-app.get('/', (req, res) => res.send('hey back!'))
+app.get('/', (req, res) => res.send('hey back! Again'))
 
 app.listen(port)
 
