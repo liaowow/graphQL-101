1243b7e8b6cf4f06ece24bc6cb7d03de888c9264
Annie Liao <liaowow@gmail.com>
2020-10-06 14:32:35 -0400

    build a minimal node:express server

---
+++ server/src/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 9000
+
+app.get('/', (req, res) => res.send('hey back!'))
+
+app.listen(port)
+
+console.log(`[app]: http://localhost:${port}`)
